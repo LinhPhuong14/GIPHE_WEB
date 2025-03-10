2640922f9e853b19865413be42ea69e33d1a7fe9
LinhPhuong14 <Linhnp@funix.edu.vn>
2025-03-10 15:07:54 +0700

    feat: admin component

---
+++ src/components/admin/admin-header.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { Bell } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { signOut } from "next-auth/react";
+import Image from "next/image";
+import { ModeToggle } from "../mode-toggle";
+
+interface DashboardHeaderProps {
+  user: {
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+  };
+}
+
+export function DashboardHeader({ user }: DashboardHeaderProps) {
+  const initials = user.name
+    ? user.name
+        .split(" ")
+        .map((name) => name.substring(0, 1))
+        .join("")
+    : "";
+  
+  return (
+    <div className="flex items-center justify-between p-4">
+      <ModeToggle />
+      <div className="flex items-center gap-4">
+        <Input className="max-w-7xl w-60" placeholder="Search..." />
+        <Button variant="ghost">
+          <Bell />
+        </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            {/* <Avatar className="border">
+              <Image src={user.image} alt={user.name} />
+              <AvatarFallback>{initials}</AvatarFallback>
+            </Avatar> */}
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Profile</DropdownMenuLabel>
+            <DropdownMenuItem>
+              <a href="/dashboard/profile">View Profile</a>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => signOut()}>
+              Sign out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
+}
